08e9d4e0ec1528bc4c3a84db0ef7f873db2fb7f3
KoryB <byrnekory@gmail.com>
2019-06-08 09:52:44 -0400

    Changed to version 2.1

---
+++ Views/Components/test.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p> {{ message }} </p>
+    </div>
+</template>
+
+<script>
+    module.exports = {
+        name: "TestComponent",
+        
+        data() {
+            return {
+                message: "Hello!!!!"
+            }
+        }
+    })
+</script>
